9387865603fbfd7f7c904d8dc033600e788e88c6
DVK-NET <denniskot@gmail.com>
2021-10-21 12:03:40 +0300

    refactored and entrypoint added

--- src/changelogcreator/main.py
+++ src/changelogcreator/main.py
@@ -7,36 +7,9 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from bs4 import BeautifulSoup
 # args.image = '8fe56664df0a'
 template_name = 'toolset.md'
-output_filename = 'changelog.md'
 
 regex = r"^FROM .*"
 
-parser = argparse.ArgumentParser(
-    description='Creates changelog file based on toolset.xml. Extracts version from docker container, using toolset.md template')
-
-parser.add_argument('--image', type=str, required=True,
-                    help='an image ID or an image name')
-
-parser.add_argument('--outpath', type=str, required=True,
-                    help='Path for generated changelog')
-parser.add_argument('--dockerfile', type=str, required=True,
-                    help='path to dockerfile')
-parser.add_argument('--mode', type=str, default='append', choices=['append', 'create'], required=False,
-                    help='either create new changelog file or append to existing one')
-args = parser.parse_args()
-
-DOCKER_FILE = Path(args.dockerfile).resolve()
-OUTPUT_FILE = Path(args.outpath).resolve()
-
-context = {} # the dict to be sent for being rendered in the template
-context['BASE_IMAGE'] = "NOT DEFINED"
-
-env = Environment(
-    loader=FileSystemLoader("templates"),
-    autoescape=select_autoescape()
-)
-
-template = env.get_template(template_name)
 
 
 def make_bold(match_obj):
@@ -44,7 +17,7 @@ def make_bold(match_obj):
         return " **" + match_obj.group(1) + "** "
 
 
-def tool_info_extractor(soup, tool, is_version_in_json=False):
+def tool_info_extractor(soup, tool, script_args, is_version_in_json=False):
     """Parse tool's name and command to get version info.
     Run extracted command in docker container 
     Returns List
@@ -60,7 +33,7 @@ def tool_info_extractor(soup, tool, is_version_in_json=False):
     out = []
     for command in soup.find_all(tool):
         cmd = command.find('command-to-check-version').text.split(" ")
-        bash_cmd = ['docker', 'run', args.image, *cmd]
+        bash_cmd = ['docker', 'run', script_args.image, *cmd]
         process = subprocess.Popen(bash_cmd, stdout=subprocess.PIPE)
         output, error = process.communicate()
         if not error:
@@ -77,29 +50,59 @@ def tool_info_extractor(soup, tool, is_version_in_json=False):
                 'version': "Not present or error"})
     return out
 
+def main():
+    parser = argparse.ArgumentParser(
+    description='Creates changelog file based on toolset.xml. Extracts version from docker container, using toolset.md template')
 
-with open(DOCKER_FILE, "r") as fp:
-    # base image info parser
-    text = fp.read()
-    from_image = re.findall('FROM (.*)', text)
+    parser.add_argument('--image', type=str, required=True,
+                        help='an image ID or an image name')
+
+    parser.add_argument('--outpath', type=str, required=True,
+                        help='Path for generated changelog')
+    parser.add_argument('--dockerfile', type=str, required=True,
+                        help='path to dockerfile')
+    parser.add_argument('--mode', type=str, default='append', choices=['append', 'create'], required=False,
+                        help='either create new changelog file or append to existing one')
+    args = parser.parse_args()
+
+    DOCKER_FILE = Path(args.dockerfile).resolve()
+    OUTPUT_FILE = Path(args.outpath).resolve()
+
+    context = {} # the dict to be sent for being rendered in the template
+    context['BASE_IMAGE'] = "NOT DEFINED"
+
+    env = Environment(
+        loader=FileSystemLoader("templates"),
+        autoescape=select_autoescape()
+    )
+
+    template = env.get_template(template_name)
+
+    with open(DOCKER_FILE, "r") as fp:
+        # base image info parser
+        text = fp.read()
+        from_image = re.findall('FROM (.*)', text)
+        if from_image:
+            context['BASE_IMAGE'] = from_image[0]
+
+    with open('toolset.xml', 'r') as fp:
+        soup = BeautifulSoup(fp, "xml")
+
+
+    context['APT_TOOLS'] = tool_info_extractor(soup, 'apt-tool', args)
+    context['PIP_TOOLS'] = tool_info_extractor(soup, 'python-pip-tool', args)
+    context['AZURE_CLI_TOOLS'] = tool_info_extractor(soup, 'azure-cli-tool', args, is_version_in_json=True )
     if from_image:
         context['BASE_IMAGE'] = from_image[0]
 
-with open('toolset.xml', 'r') as fp:
-    soup = BeautifulSoup(fp, "xml")
+    output = template.render(context)
 
+    if args.mode == 'create':
+        mode = 'w'
+    else:
+        mode = 'a'
+    with open(OUTPUT_FILE, mode) as fp:
+        fp.write(output)
 
-context['APT_TOOLS'] = tool_info_extractor(soup, 'apt-tool')
-context['PIP_TOOLS'] = tool_info_extractor(soup, 'python-pip-tool')
-context['AZURE_CLI_TOOLS'] = tool_info_extractor(soup, 'azure-cli-tool')
-if from_image:
-    context['BASE_IMAGE'] = from_image[0]
-
-output = template.render(context)
-
-if args.mode == 'create':
-    mode = 'w'
-else:
-    mode = 'a'
-with open(OUTPUT_FILE, mode) as fp:
-    fp.write(output)
+if __name__ == "__main__":
+    main()
