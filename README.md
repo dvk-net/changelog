7aca6a5ad45ecbee91fec7859d429737d229049c
DVK-NET <denniskot@gmail.com>
2021-10-21 13:35:47 +0300

    version bump to 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# changelog
-create md changelog
+# changelog python utility
+
+The utility for extracting info about installed (used) tools inside docker container
+
+## Usage
+
+1. Clone repo or 
+
+    ```bash
+    pip install -U git+<repo URL>
+    ```
+
+1. Run it
+    ```bash
+    python3 main.py ..params 
+    ```
+    or
+    ```
+    docker-changelog-creator ..params
+    ```
+    where params are:
+    ```bash
+    Creates changelog file based on toolset.xml. Extracts version from docker container, using toolset.md template
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    --image IMAGE         an image ID or an image name
+    --outpath OUTPATH     Path for generated changelog
+    --dockerfile DOCKERFILE
+                            path to dockerfile
+    --toolstotestfile TOOLSTOTESTFILE
+                            path to the xml file with tools for extracting info
+    --outputtemplatefile OUTPUTTEMPLATEFILE
+                            path to Junja2 template file for rendering and createtin well-formated changelog
+    --mode {append,create}
+                        either create new changelog file or append to existing one
+    ```
+1. Tools to testfile example
+    ```xml
+    <tools>
+        <apt-tools>
+            <apt-tool>
+                <name>Python 2</name>
+                <command-to-check-version>apt-cache show python2</command-to-check-version>
+            </apt-tool>
+            <apt-tool>
+                <name>Python 3.8</name>
+                <command-to-check-version>apt-cache show python3.8</command-to-check-version>
+            </apt-tool>
+            <apt-tool>
+                <name>Python 3.11</name>
+                <command-to-check-version>apt-cache show python3.11</command-to-check-version>
+            </apt-tool>
+        </apt-tools>
+        <python-pip-tools>
+            <python-pip-tool>
+                <name>python conan package</name>
+                <command-to-check-version>pip show conan</command-to-check-version>
+            </python-pip-tool>
+        </python-pip-tools>
+        <azure-cli-tools>
+            <azure-cli-tool>
+                <name>Azure CLI</name>
+                <command-to-check-version>az version</command-to-check-version>
+            </azure-cli-tool>
+        </azure-cli-tools>
+    </tools>
+    ```
+1. Jinja2 template example(https://jinja.palletsprojects.com/en/3.0.x/templates/):
+    ```jinja
+    # TOOLS
+
+    ## Base image: {{BASE_IMAGE}}
+
+
+
+    {% if APT_TOOLS %}
+    ## APT TOOLS
+
+    {% for pkg in APT_TOOLS %}
+    --------------------------
+    ## {{ pkg.name }}
+    <pre>{{ pkg.version|replace("\n", "<br>")|default("Not found", true) }}</pre>
+    --------------------------
+    {% endfor %}
+    {% else %}
+        NO APT TOOLS where provided...
+    {% endif %}
+
+
+    {% if PIP_TOOLS %}
+    ## APT TOOLS
+
+    {% for pkg in PIP_TOOLS %}
+    --------------------------
+    ## {{ pkg.name }}
+    <pre>{{ pkg.version|replace("\n", "<br>")|default("Not found", true) }}</pre>
+    --------------------------
+    {% endfor %}
+    {% else %}
+        NO PIP TOOLS where provided...
+    {% endif %}
+
+
+    {% if AZURE_CLI_TOOLS %}
+    ## AZURE CLI TOOLS
+
+    {% for pkg in AZURE_CLI_TOOLS %}
+    --------------------------
+    ## {{ pkg.name }}
+
+    **azure-cli**: {{pkg['version']['azure-cli']}}<br> 
+    **azure-cli-core**: {{pkg['version']['azure-cli-core']}} <br> 
+    **azure-cli-telemetry**: {{pkg['version']['azure-cli-telemetry']}} <br> 
+
+    --------------------------
+    {% endfor %}
+    {% else %}
+        NO AZURE CLI TOOLS where provided...
+    {% endif %}
+    ```
--- src/changelogcreator/main.py
+++ src/changelogcreator/main.py
@@ -3,13 +3,12 @@ import subprocess
 import re
 import json
 from pathlib import Path
+from sys import stderr
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from bs4 import BeautifulSoup
 # args.image = '8fe56664df0a'
-template_name = 'toolset.md'
 
 regex = r"^FROM .*"
-
 
 
 def make_bold(match_obj):
@@ -34,7 +33,7 @@ def tool_info_extractor(soup, tool, script_args, is_version_in_json=False):
     for command in soup.find_all(tool):
         cmd = command.find('command-to-check-version').text.split(" ")
         bash_cmd = ['docker', 'run', script_args.image, *cmd]
-        process = subprocess.Popen(bash_cmd, stdout=subprocess.PIPE)
+        process = subprocess.Popen(bash_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
         output, error = process.communicate()
         if not error:
             if not is_version_in_json:
@@ -50,9 +49,10 @@ def tool_info_extractor(soup, tool, script_args, is_version_in_json=False):
                 'version': "Not present or error"})
     return out
 
+
 def main():
     parser = argparse.ArgumentParser(
-    description='Creates changelog file based on toolset.xml. Extracts version from docker container, using toolset.md template')
+        description='Creates changelog file based on toolset.xml. Extracts version from docker container, using toolset.md template')
 
     parser.add_argument('--image', type=str, required=True,
                         help='an image ID or an image name')
@@ -61,22 +61,28 @@ def main():
                         help='Path for generated changelog')
     parser.add_argument('--dockerfile', type=str, required=True,
                         help='path to dockerfile')
+    parser.add_argument('--toolstotestfile', type=str, required=True,
+                        help='path to the xml file with tools for extracting info')
+    parser.add_argument('--outputtemplatefile', type=str, required=True,
+                        help='path to Junja2 template file for rendering and createtin well-formated changelog')
     parser.add_argument('--mode', type=str, default='append', choices=['append', 'create'], required=False,
                         help='either create new changelog file or append to existing one')
     args = parser.parse_args()
 
-    DOCKER_FILE = Path(args.dockerfile).resolve()
+    TOOLSET_FILE = Path(args.toolstotestfile).resolve()
     OUTPUT_FILE = Path(args.outpath).resolve()
-
-    context = {} # the dict to be sent for being rendered in the template
+    OUTPUTTEMPLATE_FILE = Path(args.outputtemplatefile).resolve().name
+    OUTPUTTEMPLATE_DIR = Path(args.outputtemplatefile).resolve().parent
+    DOCKER_FILE = Path(args.dockerfile).resolve()
+    context = {}  # the dict to be sent for being rendered in the template
     context['BASE_IMAGE'] = "NOT DEFINED"
 
     env = Environment(
-        loader=FileSystemLoader("templates"),
+        loader=FileSystemLoader(OUTPUTTEMPLATE_DIR),
         autoescape=select_autoescape()
     )
 
-    template = env.get_template(template_name)
+    template = env.get_template(OUTPUTTEMPLATE_FILE)
 
     with open(DOCKER_FILE, "r") as fp:
         # base image info parser
@@ -85,13 +91,14 @@ def main():
         if from_image:
             context['BASE_IMAGE'] = from_image[0]
 
-    with open('toolset.xml', 'r') as fp:
+    with open(TOOLSET_FILE, 'r') as fp:
         soup = BeautifulSoup(fp, "xml")
-
 
     context['APT_TOOLS'] = tool_info_extractor(soup, 'apt-tool', args)
     context['PIP_TOOLS'] = tool_info_extractor(soup, 'python-pip-tool', args)
-    context['AZURE_CLI_TOOLS'] = tool_info_extractor(soup, 'azure-cli-tool', args, is_version_in_json=True )
+    context['AZURE_CLI_TOOLS'] = tool_info_extractor(
+        soup, 'azure-cli-tool', args, is_version_in_json=True)
+
     if from_image:
         context['BASE_IMAGE'] = from_image[0]
 
@@ -103,6 +110,7 @@ def main():
         mode = 'a'
     with open(OUTPUT_FILE, mode) as fp:
         fp.write(output)
+
 
 if __name__ == "__main__":
     main()
